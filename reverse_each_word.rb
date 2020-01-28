d1633a0d150cb6bd841e870ed74f5bb4dbe1d5f1
Christine Bumpous <bumpouce@gmail.com>
2020-01-28 20:12:51 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word (sentence1)
+
+  words_array = sentence1.split(" ")
+  reverse_word_sentence = []
+  
+  words_array.collect do |word| 
+    letters_array = word.split(//)  
+    word = letters_array.reverse.join
+    reverse_word_sentence << word 
+  end
+  sentence1 = reverse_word_sentence.join( " ")
+end
+
