ce6cc1e256f4b26630a4deba8a25c0a815d79393
Christine Bumpous <bumpouce@gmail.com>
2020-01-28 20:13:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,12 @@ def reverse_each_word (sentence1)
   reverse_word_sentence = []
   
   words_array.collect do |word| 
-    letters_array = word.split(//)  
-    word = letters_array.reverse.join
-    reverse_word_sentence << word 
+    
+#    letters_array = word.split(//)  
+#    word = letters_array.reverse.join
+#    reverse_word_sentence << word 
+
+      pp "Collected word: #{word}"
   end
   sentence1 = reverse_word_sentence.join( " ")
 end
